e66d49f9f9b3c1ce457c5ed0822c0da38ea14d80
El0o <elodie.duflaut@gmail.com>
2024-06-12 14:49:32 +0200

    Home Page Update

---
+++ js/script.js
@@ -0,0 +1,69 @@
+var timer, slider, sliderImgs, slideWidth, cpt;
+
+
+window.onload = () => {
+    const sliderBox = document.querySelector(".slider");
+    slider = document.querySelector(".slider-imgs");
+
+    cpt = 0;
+
+    slideWidth = sliderBox.getBoundingClientRect().width;
+
+    let firstImg = slider.firstElementChild.cloneNode(true);
+    slider.appendChild(firstImg);
+
+    sliderImgs = Array.from(slider.children);
+    let next = document.querySelector(".arrow.right");
+    let prev = document.querySelector(".arrow.left");
+
+    next.addEventListener("click", slideNext);
+    prev.addEventListener("click", slidePrevious);
+
+    timer = setInterval(slideNext, 5000);
+
+    slider.addEventListener("mouseover", stopTimer);
+    slider.addEventListener("mouseout", startTimer);
+
+}
+
+// Slider
+
+function slideNext() {
+    cpt++;
+    let decal = -slideWidth * cpt;
+    slider.style.transition = "0.5s linear";
+    slider.style.transform = "translateX(" + decal + "px)";
+
+    setTimeout( function(){
+        if(cpt >= sliderImgs.length - 1) {
+            cpt = 0;
+            slider.style.transition = "unset";
+            slider.style.transform = "translateX(0)";
+        }
+    }, 400);
+
+    
+}
+
+function slidePrevious() {
+    cpt--;
+    slider.style.transition = "0.5s linear";
+
+    if(cpt < 0) {
+        cpt = sliderImgs.length - 1;
+        slider.style.transition = "unset";
+        slider.style.transform = "translateX(" + -slideWidth*cpt + "px)";
+        setTimeout(slidePrevious, 1);
+    }
+
+    let decal = -slideWidth * cpt;
+    slider.style.transform = "translateX(" + decal + "px)";
+}
+
+function stopTimer(){
+    clearInterval(timer);
+}
+
+function startTimer(){
+    timer = setInterval(slideNext, 5000);
+}
